deb46fd5cc515396f4de4d27e40f33753a0d5139
Michael Sumner <mdsumner@gmail.com>
2019-06-03 11:30:57 +0000

    --amend

--- tests/testthat/test-proj.R
+++ tests/testthat/test-proj.R
@@ -13,3 +13,23 @@ expect_equal(family_proj("lcc", clon = 147, clat = -42, secant_range = c(-70, -1
 
 
 })
+
+
+test_that("mesh points works", {
+  expect_equivalent(mesh_points(extent(-15.5, -0.5, 0, 15)),
+  as.matrix(expand.grid(-15:-1, seq(14.5, 0.5, by = -1))))
+})
+
+#set.seed(2)
+#dput(automap_nothing())
+
+test_that("auto nothing works", {
+  set.seed(2)
+  expect_equivalent(automap_nothing(),
+                    structure(c(-113.120607898551, -9.12177578104636, 22.8314257148454,
+                                64.9031664206073, 148.363627839754, 165.527071137363, -52.1624040772778,
+                                -68.0054312106626, -50.3706465095109, -52.1588634960823, -72.053785265492,
+                                -53.2950798169202), .Dim = c(6L, 2L), .Dimnames = list(NULL,
+                                                                                       c("x", "y"))),
+                    tolerance = 0.001)
+})
