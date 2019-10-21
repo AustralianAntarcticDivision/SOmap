c2f45695c4b5c31e14d3080bb8c844e4b92d44cf
Ben Raymond <ben.raymond@aad.gov.au>
2019-10-21 14:49:54 +1100

    namespace fixes

--- R/SObin.R
+++ R/SObin.R
@@ -19,16 +19,16 @@
 #' a <- SOmap_auto()
 #' a
 #' pts <- geosphere::randomCoordinates(1e6)
-#' bin <- SObin(pts[,1], pts[,2], add = TRUE)
-SObin <- function(x, y = NULL, ..., col = viridis::viridis(26), dim = c(300, 300), add = TRUE) {
+#' bin <- SObin(pts[, 1], pts[, 2], add = TRUE)
+SObin <- function(x, y = NULL, ..., col = viridisLite::viridis(26), dim = c(300, 300), add = TRUE) {
   SObj <- SOproj(x = x, y= y, target = NULL, source = NULL)
 
   crs <- SOcrs()
   ex <- spex::spex(crs = crs)
   r <- raster::raster(ex, nrows = dim[1], ncols = dim[2])
   r <- raster::setValues(r, NA_integer_)
-  tib <- tibble::tibble(cell = raster::cellFromXY(r, coordinates(SObj)))
-  summ <- dplyr::summarize(dplyr::group_by(tib, .data$cell) ,count = dplyr::n())
+  tib <- tibble::tibble(cell = raster::cellFromXY(r, sp::coordinates(SObj)))
+  summ <- dplyr::summarize(dplyr::group_by(tib, .data$cell), count = dplyr::n())
   summ <- dplyr::filter(summ, !is.na(.data$cell))
   r[summ$cell] <- summ$count
   if (add) plot(r, add = TRUE, ..., col = col)
