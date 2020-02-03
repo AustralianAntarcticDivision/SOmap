c245a379dd62aca321a4f82018898f50d466e456
Ben Raymond <ben.raymond@aad.gov.au>
2020-02-04 10:35:39 +1100

    make SOauto_crop actuall crop to the visible map extent, amongst other changes

---
+++ tests/testthat/test-autocrop.R
@@ -0,0 +1,44 @@
+context("test-autocrop")
+test_that("SOauto_crop works", {
+    ## maps of different extent
+    x30 <- SOmap(trim = -30)
+    x60 <- SOmap(trim = -60)
+    x85 <- SOmap(trim = -85)
+
+    ## crop a SpatialPolygonsDataFrame
+    chk <- SOauto_crop(SOmap_data$CCAMLR_SSRU, x30)
+    ## Spatial by default
+    expect_is(chk, "Spatial")
+    ## in this case, nothing should have been cropped
+    expect_equal(nrow(chk), nrow(SOmap_data$CCAMLR_SSRU))
+    chk <- SOauto_crop(SOmap_data$CCAMLR_SSRU, x30, sp = FALSE)
+    expect_is(chk, "sf")
+    expect_true(all(sf::st_geometry_type(chk) == "POLYGON"))
+
+    ## crop a SpatialLinesDataFrame
+    chk <- SOauto_crop(SOmap_data$fronts_orsi, x60)
+    ## removed some features
+    expect_lt(nrow(chk), nrow(SOmap_data$fronts_orsi))
+    ## as sf
+    chk <- SOauto_crop(SOmap_data$fronts_orsi, x60, sp = FALSE)
+    expect_true(all(sf::st_geometry_type(chk) == "MULTILINESTRING"))
+
+    ## crop a LINESTRING sf object
+    expect_true(all(sf::st_geometry_type(SOmap_data$ACC_FRONTS) == "LINESTRING"))
+    chk <- SOauto_crop(SOmap_data$ACC_FRONTS, x60)
+    expect_lt(nrow(chk), nrow(SOmap_data$ACC_FRONTS))
+    expect_is(chk, "SpatialLinesDataFrame")
+    chk <- SOauto_crop(SOmap_data$ACC_FRONTS, x60, sp = FALSE)
+    expect_is(chk, "sf")
+    ## LINESTRING comes back as MULTILINESTRING
+    expect_true(all(sf::st_geometry_type(chk) == "MULTILINESTRING"))
+
+    ## cropping returns empty object
+    chk <- SOauto_crop(SOmap_data$EEZ, x85, sp = FALSE)
+    ## expect sf object with no rows
+    expect_is(chk, "sf")
+    expect_equal(nrow(chk), 0L)
+    chk <- SOauto_crop(SOmap_data$EEZ, x85, sp = TRUE)
+    ## expect NULL, because we can't coerce a zero-row sf object to Spatial
+    expect_null(chk)
+})
