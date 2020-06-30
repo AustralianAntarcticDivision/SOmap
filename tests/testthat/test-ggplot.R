0802e15cbfad540cc0bfa6e185d6dfe4064048cf
Ben Raymond <ben.raymond@aad.gov.au>
2020-06-30 18:26:15 +1000

    re-enable gg tests, but without the slow domains layer

--- tests/testthat/test-ggplot.R
+++ tests/testthat/test-ggplot.R
@@ -7,14 +7,15 @@ test_that("SOgg returns a ggplot object", {
     expect_identical(sort(names(pg)), c("axis_labels", "bathy", "bathy_legend", "border", "coastline", "coord", "ice", "init", "plot_sequence", "projection", "scale_fill", "straight", "target", "theme", "trim"))
     expect_s3_class(plot(pg), "ggplot")
 
+    ## test without the ccamlr and domains layers, they are super slow
     pm <- SOmanagement(trim = -45, mpa = TRUE, mpa_labels = TRUE,
                 ccamlr = TRUE, ccamlr_labels = TRUE, ssru = TRUE, ssru_labels = TRUE,
                 ssmu = TRUE, ssmu_labels = TRUE, rb = TRUE, rb_labels = TRUE,
                 sprfmorb = TRUE, eez = TRUE, eez_labels = TRUE,
-                domains = TRUE, domains_labels = TRUE, iwc = TRUE, iwc_labels = TRUE)
+                domains = FALSE, domains_labels = FALSE, iwc = TRUE, iwc_labels = TRUE)
     expect_warning(pg <- SOgg(pm)) ## warning because we aren't also supplying basemap or a SOmap object to get target etc from
     expect_s3_class(pg, "SOmanagement_gg")
-    expect_identical(sort(names(pg)), c("ccamlr_planning_domains", "ccamlr_ssmu", "ccamlr_ssru", "ccamlr_statistical_areas", "eez", "iwc", "mpa", "plot_sequence", "research_blocks", "sprfmo_research_blocks"))
+    expect_identical(sort(names(pg)), c("ccamlr_ssmu", "ccamlr_ssru", "ccamlr_statistical_areas", "eez", "iwc", "mpa", "plot_sequence", "research_blocks", "sprfmo_research_blocks")) ##"ccamlr_planning_domains",
 
     ## things in pm should be of expected classes
     expect_is(pm$plot_sequence, "character")
@@ -26,22 +27,22 @@ test_that("SOgg returns a ggplot object", {
     }
 
     ## gg-ify p together with pm
-   # pg <- SOgg(p, pm) ## no warning this time
-   #    expect_s3_class(pg, "SOmap_gg")
-   #    expect_identical(sort(names(pg)), c("axis_labels", "bathy", "bathy_legend", "border", "ccamlr_planning_domains", "ccamlr_ssmu", "ccamlr_ssru", "ccamlr_statistical_areas", "coastline", "coord", "eez", "ice", "init", "iwc", "mpa", "plot_sequence", "projection", "research_blocks", "scale_fill", "sprfmo_research_blocks", "straight", "target", "theme", "trim"))
-   #    expect_s3_class(plot(pg), "ggplot")
+    pg <- SOgg(p, pm) ## no warning this time
+    expect_s3_class(pg, "SOmap_gg")
+    expect_identical(sort(names(pg)), c("axis_labels", "bathy", "bathy_legend", "border", "ccamlr_ssmu", "ccamlr_ssru", "ccamlr_statistical_areas", "coastline", "coord", "eez", "ice", "init", "iwc", "mpa", "plot_sequence", "projection", "research_blocks", "scale_fill", "sprfmo_research_blocks", "straight", "target", "theme", "trim")) ##"ccamlr_planning_domains",
+    expect_s3_class(plot(pg), "ggplot")
 
     ## things in pg should be of expected classes
-    # expect_is(pg$target, "Raster")
-    # expect_is(pg$plot_sequence, "character")
-    # expect_is(pg$projection, "character")
-    # expect_is(pg$straight, "logical")
-    # expect_is(pg$trim, "numeric")
-    # ## everything else in p should be a list of SO_plotter objects
-    # for (cmp in setdiff(names(pg), c("target", "plot_sequence", "projection", "straight", "trim"))) {
-    #     expect_is(pg[[cmp]], "list")
-    #     expect_true(all(vapply(pg[[cmp]], inherits, "SO_plotter", FUN.VALUE = TRUE)))
-    # }
+    expect_is(pg$target, "Raster")
+    expect_is(pg$plot_sequence, "character")
+    expect_is(pg$projection, "character")
+    expect_is(pg$straight, "logical")
+    expect_is(pg$trim, "numeric")
+    ## everything else in p should be a list of SO_plotter objects
+    for (cmp in setdiff(names(pg), c("target", "plot_sequence", "projection", "straight", "trim"))) {
+        expect_is(pg[[cmp]], "list")
+        expect_true(all(vapply(pg[[cmp]], inherits, "SO_plotter", FUN.VALUE = TRUE)))
+    }
 
     ## should get the same thing from SOmap2 directly
     ## use the full set of management layer options here, just so all code is exercised
@@ -49,15 +50,15 @@ test_that("SOgg returns a ggplot object", {
                 ccamlr = TRUE, ccamlr_labels = TRUE, ssru = TRUE, ssru_labels = TRUE,
                 ssmu = TRUE, ssmu_labels = TRUE, rb = TRUE, rb_labels = TRUE,
                 sprfmorb = TRUE, eez = TRUE, eez_labels = TRUE,
-                domains = TRUE, domains_labels = TRUE, iwc = TRUE, iwc_labels = TRUE)
-   # pg2 <- SOgg(p2)
-  #    expect_setequal(names(pg2), names(pg))
+                domains = FALSE, domains_labels = FALSE, iwc = TRUE, iwc_labels = TRUE)
+    pg2 <- SOgg(p2)
+    expect_setequal(names(pg2), names(pg))
 })
 
-# test_that("SOgg works on SOmap_auto objects", {
-#     p <- SOmap_auto(c(0, 50), c(-70, -50))
-#    # pg <- SOgg(p)
-#     expect_s3_class(pg, "SOmap_auto_gg")
-#     expect_s3_class(plot(pg), "ggplot")
-# })
+test_that("SOgg works on SOmap_auto objects", {
+    p <- SOmap_auto(c(0, 50), c(-70, -50))
+    pg <- SOgg(p)
+    expect_s3_class(pg, "SOmap_auto_gg")
+    expect_s3_class(plot(pg), "ggplot")
+})
 
