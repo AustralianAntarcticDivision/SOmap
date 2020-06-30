6939135ecbe4ac87fe71a0406b056620a20904c0
Ben Raymond <ben.raymond@aad.gov.au>
2020-07-01 08:01:17 +1000

    upgrading raster needs to be from GH temporarily

--- R/zzz.R
+++ R/zzz.R
@@ -1,7 +1,7 @@
 .onLoad <- function(libname, pkgname) {
     ## raster 3.3.6 and 3.3.7 had a bug with trim that causes our SOmap layout to fail
     if (packageVersion("raster") == "3.3.6" || packageVersion("raster") == "3.3.7") {
-        warning("your version of the raster package (", packageVersion("raster"), ") has a bug, please upgrade it with install.packages(\"raster\")")
+        warning("your version of the raster package (", packageVersion("raster"), ") has a bug, please upgrade it with install_github(\"rspatial/raster\") or install.packages(\"raster\")")
     }
     invisible()
 }
