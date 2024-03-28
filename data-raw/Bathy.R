6386ab2aaad04168251d35e629eb0d7731cc6b22
Michael Sumner <mdsumner@gmail.com>
2024-03-28 08:08:08 +0000

    re create

--- data-raw/Bathy.R
+++ data-raw/Bathy.R
@@ -1,11 +1,12 @@
 prj <- "+proj=stere +lat_0=-90 +lat_ts=-71 +lon_0=0 +k=1 +x_0=0 +y_0=0 +datum=WGS84 +units=m +no_defs +ellps=WGS84 +towgs84=0,0,0"
-src <- readAll(raadtools::readtopo("gebco_14"))  ##, xylim = extent(-180, 180, -90, 0))
-
-r <- raster(projectExtent(raster(extent(-180, 180, -90, -5), crs  = proj_longlat()), prj))
+#src <- readAll(raadtools::readtopo("gebco_14"))  ##, xylim = extent(-180, 180, -90, 0))
+src <- "/vsicurl/https://gebco2023.s3.valeria.science/gebco_2023_land_cog.tif"
+r <- raster(projectExtent(raster(extent(-180, 180, -90, -5), crs  = "EPSG:4326"), prj))
 ## cleanup and rebuild
 r <- raster(spex::buffer_extent(r, 16000), crs = prj)
 res(r) <- 16000
-Bathy <- projectRaster(src, r)
+Bathy <- raster(terra::project(terra::rast(src), terra::rast(r), by_util = TRUE))
+
 dataType(Bathy) <- "INT2S"
 Bathy <- setValues(Bathy, as.integer(values(Bathy)))
 
